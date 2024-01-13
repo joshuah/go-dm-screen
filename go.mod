1be80e0c37a7e007dd6d078abd3c4dec2dc55283
Joshua <joshuaheiks@gmail.com>
2024-01-13 14:30:06 -0500

    Updated version of go-dmlib

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/joshuah/go-dm-screen
+
+go 1.21.5
+
+require github.com/joshuah/go-dmlib v0.1.1
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	godmlib "github.com/joshuah/go-dmlib"
+)
+
+func main() {
+	fmt.Println("Ok")
+	fmt.Println(godmlib.Hello())
+	fmt.Println(godmlib.Goodbye())
+
+}
